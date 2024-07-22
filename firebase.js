419d1b6988608cf3b8fb97349e3545b84cfa62d6
rizwan <118995669+riz-33@users.noreply.github.com>
2024-07-23 03:05:31 +0500

    todo

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ import {
     updateProfile, updateEmail, updatePassword, updatePhoneNumber, GoogleAuthProvider, signInWithPopup,
 } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js';
 
-import { doc, setDoc, getDoc, getFirestore, collection, addDoc, updateDoc  }
+import { doc, setDoc, getDoc, getFirestore, addDoc, updateDoc, collection, onSnapshot, query  }
     from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js';
 
 // Your web app's Firebase configuration
@@ -30,5 +30,5 @@ const db = getFirestore(app);
 export {
     auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile,
     updateEmail, updatePassword, updatePhoneNumber, googleProvider, signInWithPopup, GoogleAuthProvider, doc, setDoc,
-    db, getDoc, collection, addDoc, updateDoc 
+    db, getDoc, collection, addDoc, updateDoc, onSnapshot, query
 }
--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-import { auth, signOut } from "./firebase.js";
+import { auth, signOut, addDoc, collection, db, onSnapshot, query } from "./firebase.js";
 
 let logout = () => {
     signOut(auth).then(() => {
@@ -12,3 +12,30 @@ let logout = () => {
 let logoutBtn = document.getElementById("logoutBtn");
 
 logoutBtn.addEventListener("click", logout);
+
+let addTodo = async () => {
+    let todo = document.getElementById("todo");
+    const docRef = await addDoc(collection(db, "todos"), {
+        value: todo.value,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    console.log(todo.value)
+}
+
+let addTodoBtn = document.getElementById("addTodoBtn");
+
+addTodoBtn && addTodoBtn.addEventListener('click', addTodo)
+
+
+let getAllTodos = async () => {
+    const ref = (collection(db, "todos"));
+    const todoList = document.getElementById("todoList");
+    const unsubscribe = onSnapshot(ref, (querySnapshot) => {
+        todoList.innerHTML = "";
+        querySnapshot.forEach((doc) => {
+            todoList.innerHTML += `<li class="list-group-item">${doc.data().value}</li>`;
+        });
+    });
+}
+
+getAllTodos();
